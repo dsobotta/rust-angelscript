b0b2d41e75a36afe3367c6b27d7bb88753ebd546
Dusten Sobotta <dusten@sentinel-engine.org>
2020-01-29 03:13:01 -0800

    formalized rust wrapper for get_library_version and get_library_options angelscript functions

--- angelscript-testbed/src/main.rs
+++ angelscript-testbed/src/main.rs
@@ -1,16 +1,58 @@
-use std::os::raw::c_char;
-use std::ffi::CStr;
 
-extern { 
-    #[link(name="angelscript_c", kind="static")]
-    fn asGetLibraryVersion() -> *const c_char;
-}
+
+// engine
+// AS_API asIScriptengine *asCreateScriptengine(asDWORD version);
+// AS_API const char      *asGetLibraryVersion();
+// AS_API const char      *asGetLibraryOptions();
+
+pub mod angelscript {
+
+    pub mod engine {
+
+        use std::os::raw::c_char;
+        use std::ffi::CStr;
+
+        //pub struct Scriptengine {
+            //asIScriptengine: *i32,
+        //}
+
+        fn read_cstring(c_buf: *const c_char) -> String {
+
+            let c_str: &CStr = unsafe { CStr::from_ptr(c_buf) };
+            let str_slice: &str = c_str.to_str().unwrap();
+            let str_buf: String = str_slice.to_owned();
+        
+            return str_buf;
+        }
+
+        pub fn get_library_version() -> String {
+            extern { 
+                #[link(name="angelscript_c", kind="static")]
+                fn asGetLibraryVersion() -> *const c_char;
+            }
+    
+            let c_buf: *const c_char = unsafe { asGetLibraryVersion() };
+            return read_cstring(c_buf);
+        }
+
+        pub fn get_library_options() -> String {
+            extern { 
+                #[link(name="angelscript_c", kind="static")]
+                fn asGetLibraryOptions() -> *const c_char;
+            }
+    
+            let c_buf: *const c_char = unsafe { asGetLibraryOptions() };
+            return read_cstring(c_buf);
+        }
+
+    } //end engine
+} // end angelscript
 
 fn main() {
-    let c_buf: *const c_char = unsafe { asGetLibraryVersion() };
-    let c_str: &CStr = unsafe { CStr::from_ptr(c_buf) };
-    let str_slice: &str = c_str.to_str().unwrap();
-    let str_buf: String = str_slice.to_owned();  // if necessary
 
-    println!("Angelscript Version: {}", str_buf);
+    let as_version: String = angelscript::engine::get_library_version();
+    println!("Angelscript Version: {}", as_version);
+
+    let as_options: String = angelscript::engine::get_library_options();
+    println!("Angelscript Library Options: {}", as_options);
 }
